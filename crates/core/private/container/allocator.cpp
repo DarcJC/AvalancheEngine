240ad21c7faa916363025c4d321cdf4bb88bd542
DarcJC <me@darc.pro>
2024-08-17 12:23:44 +0800

    (allocator) Fix clang warning

--- crates/core/private/container/allocator.cpp
+++ crates/core/private/container/allocator.cpp
@@ -3,14 +3,15 @@
 avalanche::bad_alloc::bad_alloc(): simple_error("Bad alloc") {}
 
 void * avalanche::allocate_memory(const size_t bytes, const void *hint) {
-    if (bytes > std::numeric_limits<size_t>::max())
+    if (bytes > std::numeric_limits<size_t>::max()) {
         raise_exception(bad_alloc());
+    }
 
-        if (auto p = std::malloc(bytes))
-            return p;
+    if (void* p = std::malloc(bytes))
+        return p;
 
-        raise_exception(bad_alloc());
-        return nullptr; // mute control flow analyser
+    raise_exception(bad_alloc());
+    return nullptr; // mute control flow analyser
 }
 
 void avalanche::deallocate_memory(void *pointer, size_t bytes) {
